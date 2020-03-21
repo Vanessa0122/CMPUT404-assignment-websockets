a27ba91be1a461a843254ee3be8ca7114fd7304a
Vanessa0122 <yp4@ualberta.ca>
2020-03-21 16:36:41 -0600

    still need to update read me

--- sockets.py
+++ sockets.py
@@ -23,6 +23,7 @@ import json
 import os
 
 app = Flask(__name__)
+app._static_folder = os.path.join(os.getcwd(), "static/")
 sockets = Sockets(app)
 app.debug = True
 
@@ -61,28 +62,49 @@ class World:
 
 myWorld = World()        
 
+
+sockets_list = []
+
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    for socket in sockets_list:
+        if (socket.closed):
+            sockets_list.remove(socket)
+        else:
+            raw_data = {}
+            raw_data[entity] = data
+            socket.send(json.dumps(raw_data))
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect("/static/index.html")
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
+    while not ws.closed:
+        message = ws.receive()
+        try:
+            if message:
+                message = json.loads(message)
+                keys = message.keys()
+                for key in keys:
+                    myWorld.set(key, message[key])
+        except Exception as e:
+            print(message)
+            print(e)
     return None
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
-    # XXX: TODO IMPLEMENT ME
-    return None
-
+    sockets_list.append(ws)
+    ws.send(json.dumps(myWorld.world()))
+    read_ws(ws, None)
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -98,26 +120,26 @@ def flask_post_json():
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
-    '''update the entities via this interface'''
-    return None
+    rawData = flask_post_json()
+    for key, value in rawData.items():
+        myWorld.update(entity, key, value)
+    return flask.jsonify(rawData)
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return flask.jsonify(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
-
+    return flask.jsonify(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
-
+    myWorld.clear()
+    return flask.jsonify(myWorld.world())
 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
@@ -125,4 +147,10 @@ if __name__ == "__main__":
         and run
         gunicorn -k flask_sockets.worker sockets:app
     '''
-    app.run()
+    #app.run()
+    
+    #https://github.com/heroku-python/flask-sockets
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
